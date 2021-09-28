135537e337f73f92e7bbf9f3c4e4bf403f3269c6
Steve <simanek.s@gmx.at>
2021-09-28 10:38:08 +0200

    Add project files.

---
+++ MonsterTradingCardGame/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame
+{
+    class User
+    {
+        private string _username { get; set; }
+        private string _password { get; set; }
+    }
+}
