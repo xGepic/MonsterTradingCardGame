476f18549a05c05155e9d04e7ebd1de5e39a0825
Steve <simanek.s@gmx.at>
2021-10-27 16:43:38 +0200

    Started DamageCalc

--- MonsterTradingCardGame/Battle/Battle.cs
+++ MonsterTradingCardGame/Battle/Battle.cs
@@ -4,19 +4,17 @@ namespace MonsterTradingCardGame
 {
     class Battle
     {
-        public Battle()
-        {
-
-        }
         public static int DamageCalc(ICard Card1, ICard Card2)
         {
+            int DamageFromCard1 = GetCardDamage(Card1, Card2);
+            int DamageFromCard2 = GetCardDamage(Card2, Card1);
             Console.WriteLine("##############################\n");
             if ((Card1.MyCardtype == CardType.Monster && Card2.MyCardtype == CardType.Spell) || Card1.MyCardtype == CardType.Spell && Card2.MyCardtype == CardType.Monster)
             {
                 Console.WriteLine($"{Card1.Name} vs {Card2.Name}\n");
                 Console.WriteLine($"{Card1.Damage} vs {Card2.Damage}\n");
                 Console.WriteLine("##############################\n");
-                if(Card1.Damage < Card2.Damage)
+                if (DamageFromCard1 < DamageFromCard2)
                 {
                     return 2;
                 }
@@ -30,7 +28,7 @@ namespace MonsterTradingCardGame
                 Console.WriteLine($"{Card1.Name} vs {Card2.Name}\n");
                 Console.WriteLine($"{Card1.Damage} vs {Card2.Damage}\n");
                 Console.WriteLine("##############################\n");
-                if (Card1.Damage < Card2.Damage)
+                if (DamageFromCard1 < DamageFromCard2)
                 {
                     return 2;
                 }
@@ -44,7 +42,7 @@ namespace MonsterTradingCardGame
                 Console.WriteLine($"{Card1.Name} vs {Card2.Name}\n");
                 Console.WriteLine($"{Card1.Damage} vs {Card2.Damage}\n");
                 Console.WriteLine("##############################\n");
-                if (Card1.Damage < Card2.Damage)
+                if (DamageFromCard1 < DamageFromCard2)
                 {
                     return 2;
                 }
@@ -54,6 +52,10 @@ namespace MonsterTradingCardGame
                 }
             }
             return -1;
+        }
+        private static int GetCardDamage(ICard card1, ICard card2)
+        {
+            return 1;
         }
     }
 }
--- MonsterTradingCardGame/Deck/IDeck.cs
+++ MonsterTradingCardGame/Deck/IDeck.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace MonsterTradingCardGame
+﻿namespace MonsterTradingCardGame
 {
     public interface IDeck
     {
