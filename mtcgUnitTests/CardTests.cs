023c6429839997d226260d3ee2cca764f1ec894a
Steve <simanek.s@gmx.at>
2022-01-06 21:34:07 +0100

    TestIsDeckEmpty and TestDecreaseCoins added

--- mtcgUnitTests/CardTests.cs
+++ mtcgUnitTests/CardTests.cs
@@ -6,6 +6,7 @@ namespace mtcgUnitTests
     class CardTests
     {
         private readonly string username = "CardTester";
+        private readonly string username2 = "CardTester2";
         private readonly string pw = "unit123";
         private readonly int elo = 1200;
         private readonly int coins = 20;
@@ -24,11 +25,39 @@ namespace mtcgUnitTests
             // ASSERT 
             Assert.AreEqual(result, newElo);
         }
+        [Test]
+        public void TestIsDeckEmpty()
+        {
+            // ARRANGE
+            DBConnector cmd = DBConnector.GetInstance();
+
+            // ACT
+            var result = cmd.IsDeckEmpty(username);
+
+            // ASSERT 
+            Assert.AreEqual(result, true);
+        }
+        [Test]
+        public void TestDecreaseCoins()
+        {
+            // ARRANGE
+            DBConnector cmd = DBConnector.GetInstance();
+
+            // ACT
+            cmd.RegisterUser(username2, pw, elo, coins);
+            var result1 = cmd.GetPlayerCoins(username2);
+            cmd.DecreaseCoinsofPlayer(username2);
+            var result2 = cmd.GetPlayerCoins(username2);
+
+            // ASSERT 
+            Assert.AreNotEqual(result1, result2);
+        }
         [OneTimeTearDown]
         public void TearDown()
         {
             DBConnector cmd = DBConnector.GetInstance();
             cmd.RemoveUser(username);
+            cmd.RemoveUser(username2);
         }
     }
 }
