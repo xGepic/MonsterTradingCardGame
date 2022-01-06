f05a7e3eb84a978e578cf8bad1e46b24b4a824d8
Steve <simanek.s@gmx.at>
2022-01-06 21:20:11 +0100

    TestUpdateElo added

---
+++ mtcgUnitTests/CardTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using MonsterTradingCardGame;
+
+namespace mtcgUnitTests
+{
+    class CardTests
+    {
+        private readonly string username = "CardTester";
+        private readonly string pw = "unit123";
+        private readonly int elo = 1200;
+        private readonly int coins = 20;
+        private readonly int newElo = 1000;
+        [Test]
+        public void TestUpdateElo()
+        {
+            // ARRANGE
+            DBConnector cmd = DBConnector.GetInstance();
+
+            // ACT
+            cmd.RegisterUser(username, pw, elo, coins);
+            cmd.UpdatePlayerElo(username, newElo);
+            var result = cmd.GetPlayerElo(username);
+
+            // ASSERT 
+            Assert.AreEqual(result, newElo);
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            DBConnector cmd = DBConnector.GetInstance();
+            cmd.RemoveUser(username);
+        }
+    }
+}
